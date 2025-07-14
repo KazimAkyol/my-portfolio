996749ee4ad53c1139d44b384975d7cbe1929405
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-14 17:25:45 +0200

    updated vite.config.ts

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
     plugins: [
+        react(),
         tailwindcss(),
     ],
+    build: {
+        outDir: 'dist', // Vercel'in beklediği çıktı dizini
+    },
+    server: {
+        port: 3000, // Vercel'in default portu
+    }
 })
